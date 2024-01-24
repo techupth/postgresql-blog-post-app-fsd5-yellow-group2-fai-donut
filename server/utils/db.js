1b4ef768fbb9667e64a2fdacdda9c9a4347e922e
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2024-01-24 13:22:34 +0700

    feat:postgresql

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,10 @@
 // Creating PostgreSQL Client here
+// โค้ดนี้อยู่ในไฟล์ server/utils/db.js
+import * as pg from "pg";
+const { Pool } = pg.default;
+
+const pool = new Pool({
+  connectionString: "postgresql://postgres:314257@127.0.0.1:5432/posts",
+});
+
+export { pool };
